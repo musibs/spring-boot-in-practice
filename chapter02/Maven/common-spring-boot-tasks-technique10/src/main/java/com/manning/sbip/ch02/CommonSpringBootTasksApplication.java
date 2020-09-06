45fce345bbef85bdde2cb3b6ae0ed7b8817d5dea
musibs <musib.somnath@gmail.com>
2020-09-06 19:19:30 +0530

    Removed additional dependencies from the build files

--- chapter02/Maven/common-spring-boot-tasks-technique10/src/main/java/com/manning/sbip/ch02/CommonSpringBootTasksApplication.java
+++ chapter02/Maven/common-spring-boot-tasks-technique10/src/main/java/com/manning/sbip/ch02/CommonSpringBootTasksApplication.java
@@ -9,5 +9,4 @@ public class CommonSpringBootTasksApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommonSpringBootTasksApplication.class, args);
     }
-
 }
