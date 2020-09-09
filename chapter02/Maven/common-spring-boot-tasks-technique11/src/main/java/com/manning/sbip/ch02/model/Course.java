f31979d377c941ad7d3e260a2ae40bff61667cf5
musibs <musib.somnath@gmail.com>
2020-09-10 02:11:05 +0530

    Added the updated Technique 11 final Maven project

--- chapter02/Maven/common-spring-boot-tasks-technique11/src/main/java/com/manning/sbip/ch02/model/Course.java
+++ chapter02/Maven/common-spring-boot-tasks-technique11/src/main/java/com/manning/sbip/ch02/model/Course.java
@@ -1,20 +1,22 @@
 package com.manning.sbip.ch02.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Course {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
 	private String description;
 
 	public Course() {}
 
-	public Course(long id, String name, String description) {
-		this.id = id;
+	public Course(String name, String description) {
 		this.name = name;
 		this.description = description;
 	}
--- chapter02/Maven/common-spring-boot-tasks-technique11/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS Course;
-CREATE TABLE Course
-(
-    id int(15) NOT NULL,
-    name varchar(100) NOT NULL,
-    description varchar(1000) NOT NULL,
-    PRIMARY KEY (id)
-);
