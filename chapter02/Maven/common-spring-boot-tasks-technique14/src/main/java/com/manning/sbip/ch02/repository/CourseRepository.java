03e9995dc60cd76e9d326abab0ef4b7334faaf1c
musibs <musib.somnath@gmail.com>
2020-09-18 18:53:13 +0530

    Added the final Maven project for Technique 14 - @Query annotation example

---
+++ chapter02/Maven/common-spring-boot-tasks-technique14/src/main/java/com/manning/sbip/ch02/repository/CourseRepository.java
@@ -0,0 +1,25 @@
+package com.manning.sbip.ch02.repository;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.manning.sbip.ch02.model.Course;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface CourseRepository extends CrudRepository<Course, Long> {
+
+    @Query("select c from Course c where c.category=?1")
+    Iterable<Course> findAllByCategory(String category);
+
+    @Query("select c from Course c where c.category=:category and c.rating >:rating")
+    Iterable<Course> findAllByCategoryAndRatingGreaterThan(@Param("category") String category,
+                                                           @Param("rating") int rating);
+    @Modifying
+    @Transactional
+    @Query("update Course c set c.rating=:rating where c.name=:name")
+    int updateCourseRatingByName(@Param("rating") int rating, @Param("name") String name);
+}
