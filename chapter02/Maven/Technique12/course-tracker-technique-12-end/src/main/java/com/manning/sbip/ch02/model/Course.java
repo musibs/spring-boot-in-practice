bee27d0ddabf0aa582c023c59ee5f5f0fa44e058
musibs <musib.somnath@gmail.com>
2020-10-29 12:28:23 +0530

    Chapter 02 Technique 13 Maven project

--- chapter02/Maven/Technique12/course-tracker-technique-12-end/src/main/java/com/manning/sbip/ch02/model/Course.java
+++ chapter02/Maven/Technique12/course-tracker-technique-12-end/src/main/java/com/manning/sbip/ch02/model/Course.java
@@ -1,8 +1,7 @@
 package com.manning.sbip.ch02.model;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Max;
 
 public class Course {
 
--- chapter02/Maven/Technique13/course-tracker-technique-13-end/src/main/java/com/manning/sbip/ch02/CourseTrackerApplication.java
+++ chapter02/Maven/Technique13/course-tracker-technique-13-end/src/main/java/com/manning/sbip/ch02/CourseTrackerApplication.java
@@ -1,6 +1,7 @@
 package com.manning.sbip.ch02;
 
-import com.manning.sbip.ch02.model.Course;
+import com.manning.sbip.ch02.model.User;
+import com.manning.sbip.ch02.validation.PasswordRuleValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -16,18 +17,23 @@ import java.util.Set;
 public class CourseTrackerApplication implements CommandLineRunner {
 
     private static Logger logger = LoggerFactory.getLogger(CourseTrackerApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(CourseTrackerApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Course course = new Course();
-        course.setId(1L);
-        course.setRating(0);
-
+        User user1 = new User("sbip01", "sbip");
         Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-        Set<ConstraintViolation<Course>> violations = validator.validate(course);
+        Set<ConstraintViolation<User>> violations = validator.validate(user1);
         violations.forEach(courseConstraintViolation -> logger.error("A constraint violation has occurred. Violation details: [{}].", courseConstraintViolation));
+
+        User user2 = new User("sbip01", "Sbip01$4UDfg");
+        violations = validator.validate(user2);
+        if(violations.isEmpty()) {
+            logger.info("Password for user2 adhere to the password policy");
+        }
+
     }
 }
