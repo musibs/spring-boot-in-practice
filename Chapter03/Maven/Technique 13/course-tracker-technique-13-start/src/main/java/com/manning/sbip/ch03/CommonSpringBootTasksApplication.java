0e570bdfac29f7d1ebbb51b858fd6c8e030d23ee
musibs <musib.somnath@gmail.com>
2020-11-11 15:40:55 +0530

    Added Chapter 03 Technique 13 Maven project

---
+++ Chapter03/Maven/Technique 13/course-tracker-technique-13-start/src/main/java/com/manning/sbip/ch03/CommonSpringBootTasksApplication.java
@@ -0,0 +1,13 @@
+package com.manning.sbip.ch03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonSpringBootTasksApplication {
+
+   public static void main(String[] args) {
+        SpringApplication.run(CommonSpringBootTasksApplication.class, args);
+    }
+
+}
