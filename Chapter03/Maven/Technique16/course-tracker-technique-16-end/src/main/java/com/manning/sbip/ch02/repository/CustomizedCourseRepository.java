faf424ea3e18c9da1733c07c55758077f4a5c35d
musibs <musib.somnath@gmail.com>
2020-11-13 12:53:15 +0530

    Updated chapter 03 Maven projects

--- Chapter03/Maven/Technique16/course-tracker-technique-16-end/src/main/java/com/manning/sbip/ch02/repository/CustomizedCourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.manning.sbip.ch02.repository;
-
-import com.manning.sbip.ch02.model.Course;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomizedCourseRepository extends BaseRepository<Course, Long> {
-}
