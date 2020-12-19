6d54112cd40ee6c4d47a9ef16e04da615f6021b8
musibs <musib.somnath@gmail.com>
2020-12-19 19:15:20 +0530

    Added Technique 28 Maven Project

--- chapter04/Maven/Technique28/course-tracker-technique-28-end/src/main/java/com/manning/sbip/ch04/security/SecurityConfiguration.java
+++ chapter04/Maven/Technique28/course-tracker-technique-28-end/src/main/java/com/manning/sbip/ch04/security/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,14 +19,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/login").permitAll()
-                .anyRequest().authenticated().and().formLogin().loginPage("/login");
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/webjars/**", "/images/*", "/css/*");
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
     }
 
     @Override
--- chapter04/Maven/Technique28/course-tracker-technique-28-start/src/main/java/com/manning/sbip/ch04/security/SecurityConfiguration.java
+++ chapter04/Maven/Technique28/course-tracker-technique-28-start/src/main/java/com/manning/sbip/ch04/security/SecurityConfiguration.java
@@ -20,9 +20,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/login").permitAll()
-                .anyRequest().authenticated().and().formLogin().loginPage("/login");
+
     }
 
     @Override
