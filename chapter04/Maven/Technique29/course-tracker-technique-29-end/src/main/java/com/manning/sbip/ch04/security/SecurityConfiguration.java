78d8d73413439027ef0e6db6dda8f5f9bd39c679
musibs <musib.somnath@gmail.com>
2020-12-20 08:33:15 +0530

    Added Technique 29 Maven Project

---
+++ chapter04/Maven/Technique29/course-tracker-technique-29-end/src/main/java/com/manning/sbip/ch04/security/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.manning.sbip.ch04.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/adduser", "/login", "/login-error", "/login-verified").permitAll()
+                .anyRequest().authenticated().and().formLogin().loginPage("/login").failureUrl("/login-error");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/webjars/**", "/images/*", "/css/*");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
