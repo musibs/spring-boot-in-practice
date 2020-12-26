67bd3b834ea22c805aa4928b5e9b0eaef5f21568
musibs <musib.somnath@gmail.com>
2020-12-26 22:16:39 +0530

    Added the Maven project for Technique 32

--- chapter04/Maven/Technique32/course-tracker-technique-32-end/src/main/java/com/manning/sbip/ch04/controller/RegistrationController.java
+++ chapter04/Maven/Technique32/course-tracker-technique-32-end/src/main/java/com/manning/sbip/ch04/controller/RegistrationController.java
@@ -1,19 +1,23 @@
 package com.manning.sbip.ch04.controller;
 
+import com.manning.sbip.ch04.dto.RecaptchaDto;
 import com.manning.sbip.ch04.dto.UserDto;
 import com.manning.sbip.ch04.event.UserRegistrationEvent;
 import com.manning.sbip.ch04.model.ApplicationUser;
 import com.manning.sbip.ch04.service.UserService;
+import com.manning.sbip.ch04.service.impl.GoogleRecaptchaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Controller
@@ -28,6 +32,9 @@ public class RegistrationController {
     @Value("${app.email.verification:N}")
     private String emailVerification;
 
+    @Autowired
+    private GoogleRecaptchaService captchaService;
+
     @GetMapping("/adduser")
     public String register(Model model) {
         model.addAttribute("user", new UserDto());
@@ -35,9 +42,18 @@ public class RegistrationController {
     }
 
     @PostMapping("/adduser")
-    public String register(@Valid @ModelAttribute("user") UserDto userDto, BindingResult result) {
+    public String register(@Valid @ModelAttribute("user") UserDto userDto, HttpServletRequest httpServletRequest, BindingResult result) {
         if(result.hasErrors()) {
             return "add-user";
+        }
+        String response = httpServletRequest.getParameter("g-recaptcha-response");
+        if(response == null) {
+            return "add-user";
+        }
+        String ip = httpServletRequest.getRemoteAddr();
+        RecaptchaDto recaptchaDto = captchaService.verify(ip, response);
+        if(!recaptchaDto.isSuccess()) {
+            return "redirect:adduser?incorrectCaptcha";
         }
 
         ApplicationUser applicationUser = userService.createUser(userDto);
